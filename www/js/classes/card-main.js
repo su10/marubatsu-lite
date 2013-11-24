8c203d709f3d61397784182d8eed47c5e9f1954a
suto <suto@KB-no-iMac.local>
2013-11-24 20:18:03 +0900

    カード選択するとピョコっと飛び出るようにした

--- www/js/classes/card-main.js
+++ www/js/classes/card-main.js
@@ -4,8 +4,13 @@
 
 CardMain = Class.create(Group, {
     cardSprite: null,
+    handCardMain: null,
     initialize: function() {
         Group.call(this);
         this.cardSprite = new CardSprite(this);
+
+        this.cardSprite.addEventListener(Event.TOUCH_END, function() {
+            this.handCardMain.selectCard(this);
+        }.bind(this));
     }
 });
--- www/js/classes/hand-card-main.js
+++ www/js/classes/hand-card-main.js
@@ -1,11 +1,13 @@
 // TODO:
 // spriteにカード追加・削除
 // IE6~8で動かないけどgetter使いたい
+// 選択状態のカードの位置を定数にする
 
 HandCardMain = Class.create(Object, {
     player: null,
     cards: null,
     handCardGroup: null,
+    selectedCard: null,
 
     initialize: function(player) {
         this.player = player;
@@ -16,6 +18,7 @@ HandCardMain = Class.create(Object, {
     addCard: function(card) {
         if(card instanceof CardMain) {
             this.cards.push(card);
+            card.handCardMain = this;
             this.handCardGroup.addCard(card.cardSprite);
         } else {
             throw 'invalid class.';
@@ -25,10 +28,21 @@ HandCardMain = Class.create(Object, {
     removeCard: function(card) {
         for(var i = 0, l = this.cards.length; i < l; i++) {
             if(card === this.cards[i]) {
+                this.cards[i].handCardMain = null;
                 this.cards.slice(i, 1);
                 this.handCardGroup.removeCard(card.cardSprite);
                 break;
             }
         }
+    },
+
+    selectCard: function(card) {
+        if(this.selectedCard === card) return;
+        if(this.selectedCard) {
+            // 位置を戻す
+            this.selectedCard.cardSprite.y = 0;
+        }
+        card.cardSprite.y -= 30;
+        this.selectedCard = card;
     }
 });
